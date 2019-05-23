e014c034185fa178eaa04f569809a5dc1c2a573a
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-05-24 09:49:36 +1200

    First Upload, All needed files are here

--- website/python.py
+++ website/python.py
@@ -1,5 +1,6 @@
-from bottle import run, route, view, get, post, request, static_file
+from bottle import run, route, view, get, post, request, template, static_file
 from itertools import count
+
 
 
 ###Pages###
@@ -8,5 +9,27 @@ from itertools import count
 @view('index')
 def index():
     pass
+
+#Product page
+@route('/products.html')
+@view('products.html')
+def products():
+    pass
+
+
+
+##Static files###
+#Images
+@route('/img/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./Images')
+#Css files
+@route('/css/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./Css')
+#Script files
+@route('/script/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./Script')
 
 run(host='0.0.0.0', port = 399, reloader = True, debug = True)
