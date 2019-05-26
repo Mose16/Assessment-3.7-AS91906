6c3dd8b5a03b3f14eddfa5e005a27ca4641ac2ce
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-05-27 11:12:00 +1200

    Ver-2  Created
    Updated Product page and python file, added CSS for product page

--- website/python.py
+++ website/python.py
@@ -1,7 +1,26 @@
-from bottle import run, route, view, get, post, request, template, static_file
-from itertools import count
+from bottle import run, route, view, get, post, request, template, static_file #Import important bottle methods
+from datetime import * #Import date time
 
-
+###Classes###
+class Bro: #Bro class
+    def __init__(self, name, description, img, cost, stock, booked_details=""): #Constructor
+        #Set bro personal variables
+        self.name = name
+        self.description = description
+        self.img = img
+        self.stock = stock
+        self.cost = cost
+        self.booked_details = booked_details
+           
+bros = [ #Test data filled with test bros
+    Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ. Will colonise only if it brings glory to his queen.","tom.jpg", 970, True),
+    Bro("Jerry","Good boi, will definatly tell us everything you did wrong. Cops might like this one.","jerry.jpg", 90, True),
+    Bro("Moses","Slightly retarded, will do anything for you. Makes you feel better about your problems. Depressed people and bullies may want.", "moses.jpg", 20, True),
+    Bro("John","Goes to the gym.","john.jpg", 100, True),
+    Bro("Liam","Will serinade. Has guitar, good boi. Ideal for girls.","liam.jpg", 100, True),
+    Bro("Fox","Will sit and play nintendo with you for as long as you like. Presence makes you feel good. Introvertes love.","laimf.jpg", 78, True),
+    Bro("Dom","Will make you feel geneticaly inferior. Has good beauty products. Gays would buy again.","dom.jpg", 20, False)
+]
 
 ###Pages###
 #Index page
@@ -14,9 +33,7 @@ def index():
 @route('/products.html')
 @view('products.html')
 def products():
-    pass
-
-
+    return dict(bros_list = bros)
 
 ##Static files###
 #Images
@@ -32,4 +49,4 @@ def server_static(filename):
 def server_static(filename):
     return static_file(filename, root='./Script')
 
-run(host='0.0.0.0', port = 399, reloader = True, debug = True)
+run(host='0.0.0.0', port = 399, reloader = True, debug = True) #Run local server
