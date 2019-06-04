457500de7704fa38fa67558cda81b4836f6be874
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-06-04 12:42:08 +1200

    Changes

--- website/python.py
+++ website/python.py
@@ -33,7 +33,7 @@ bros = [ #Test data filled with test bros
     Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ. Will colonise only if it brings glory to his queen.","tom.jpg", 970, True),
     Bro("Jerry","Good boi, will definatly tell us everything you did wrong. Cops might like this one.","jerry.jpg", 90, True),
     Bro("Moses","Slightly retarded, will do anything for you. Makes you feel better about your problems. Depressed people and bullies may want.", "moses.jpg", 20, True),
-    Bro("John","Goes to the gym.","john.jpg", 100, True),
+    Bro("John","Goes to the gym.","john.jpg", -99000, True),
     Bro("Liam","Will serinade. Has guitar, good boi. Ideal for girls.","liam.jpg", 100, True),
     Bro("Fox","Will sit and play nintendo with you for as long as you like. Presence makes you feel good. Introvertes love.","laimf.jpg", 78, True),
     Bro("Dom","Will make you feel geneticaly inferior. Has good beauty products. Gays would buy again.","dom.jpg", 20, True)
