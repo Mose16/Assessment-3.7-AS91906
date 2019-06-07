0b6bd5df8f314e7a9aeead19ac3dff390a6f64ef
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-06-07 12:18:46 +1200

    Version-6

--- website/python.py
+++ website/python.py
@@ -15,28 +15,28 @@ class Bro: #Bro class
 
 ###Arrays###     
 MONTHS = { #List of months with their numbers to help convert the dates on purchase success page
-    'Jan' : 1,
-    'Feb' : 2,
-    'Mar' : 3,
-    'Apr' : 4,
+    'January' : 1,
+    'Febuary' : 2,
+    'March' : 3,
+    'April' : 4,
     'May' : 5,
-    'Jun' : 6,
-    'Jul' : 7,
-    'Aug' : 8,
-    'Sep' : 9, 
-    'Oct' : 10,
-    'Nov' : 11,
-    'Dec' : 12
+    'June' : 6,
+    'July' : 7,
+    'August' : 8,
+    'September' : 9, 
+    'October' : 10,
+    'November' : 11,
+    'December' : 12
 }
         
 bros = [ #Test data filled with test bros
     Bro("Tom","Generic british boi. Nice and smart so don't hire him if you don't want to feel bad about your IQ. Will colonise only if it brings glory to his queen.","tom.jpg", 970, True),
     Bro("Jerry","Good boi, will definatly tell us everything you did wrong. Cops might like this one.","jerry.jpg", 90, True),
     Bro("Moses","Slightly retarded, will do anything for you. Makes you feel better about your problems. Depressed people and bullies may want.", "moses.jpg", 20, True),
-    Bro("John","Goes to the gym.","john.jpg", -99000, True),
+    Bro("John","Goes to the gym.","john.jpg", 100, True),
     Bro("Liam","Will serinade. Has guitar, good boi. Ideal for girls.","liam.jpg", 100, True),
     Bro("Fox","Will sit and play nintendo with you for as long as you like. Presence makes you feel good. Introvertes love.","laimf.jpg", 78, True),
-    Bro("Dom","Will make you feel geneticaly inferior. Has good beauty products. Gays would buy again.","dom.jpg", 20, True)
+    Bro("Dom","Will make you feel geneticaly inferior. Has good beauty products. Gays would buy again.","dom.jpg", 20, False)
 ]
 
 ###Pages###
@@ -98,7 +98,6 @@ def purchase_success():
     current_bro.booked_details = [Fname, Lname, str(curr_date.strftime("%B")) + " " + str(curr_date.day) + ", " + str(curr_date.year), date_, total_cost] #Store the booked details in the object
     return dict(bro = current_bro) #Pass object back into page
 
-
 #return_product page
 @route('/return_product.html')
 @view('return_product.html')
@@ -118,12 +117,31 @@ def return_success():
     for bro in bros:
         if bro.stock == False: 
             if bro.booked_details[0] == Fname and bro.booked_details[1] == Lname:
-                found_purchase = bro
+                found_bro = bro
                 break
-    if found_purchase == None:  
-        return dict(bro = False)
-    found_purchase.stock = True #Change stock
-    return dict(bro = found_purchase) #Return found_bro to page
+    found_bro.stock = True #Change stock
+    return dict(bro = found_bro) #Return found_bro to page
+
+#Application page
+@route('/application.html')
+@view('application.html')
+def application():
+    pass
+
+#Application success page
+@route('/application_success', method = "POST")
+@view('application_success')
+def application_success():
+    #Get form data entries
+    Fname = request.forms.get("first_name")
+    Lname = request.forms.get("last_name")
+    description = request.forms.get("description")
+    cost = int(request.forms.get("cost"))
+    
+    bros.append(Bro(Fname, description, "empty.jpg", cost, True))
+    return dict(bro = bros[-1])
+
+
 
 
 ##Static files###
