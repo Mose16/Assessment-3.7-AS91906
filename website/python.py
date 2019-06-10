df0350a96b1b8473e4d138940fc3e1836a64dd96
Mose16 <47540314+Mose16@users.noreply.github.com>
2019-06-11 10:29:31 +1200

    Updated

--- website/python.py
+++ website/python.py
@@ -50,23 +50,29 @@ def index():
 @route('/products.html')
 @view('products.html')
 def products():
-    return dict(bros_list = bros)
+    try:
+        return dict(bros_list = bros)
+    except:
+        return dict(bros_list = False)    
 
 #Purchase page
 current_bro = None #This variable is used to easily pass the currently processed bro through pages, (Purchase -> purchase success page)
 @route('/purchase/<name>')
 @view('purchase')
 def purchase(name):
-    global current_bro
-    #Find the bro by name
-    found_bro = None
-    for bro in bros:
-        if name == bro.name:
-            found_bro = bro
-            break
-    found_bro.stock = False
-    current_bro = found_bro #Set the object as current bro and return it to the page
-    return dict(bro = found_bro) #Return found_bro to page
+    try:
+        global current_bro
+        #Find the bro by name
+        found_bro = None
+        for bro in bros:
+            if name == bro.name:
+                found_bro = bro
+                break
+        found_bro.stock = False
+        current_bro = found_bro #Set the object as current bro and return it to the page
+        return dict(bro = found_bro) #Return found_bro to page
+    except:
+        return dict(bro = False)
 
 #Purchase_success page
 @route('/purchase_success', method = "POST")
@@ -86,16 +92,12 @@ def purchase_success():
         curr_date = datetime.now()
         d0 = date(curr_date.year, curr_date.month, curr_date.day)
         d1 = date(int(date_alt[2]), int(MONTHS[date_alt[0]]), int(date_alt[1]))
-        delta = d1 - d0
-        
-        if delta * -1 == abs(delta): #If user selected a past date then make the year go up by one
-            d0 = date(curr_date.year, curr_date.month, curr_date.day)
-            d1 = date(int(date_alt[2]) + 1, int(MONTHS[date_alt[0]]), int(date_alt[1]))
-            delta = d1 - d0        
+        delta = d1 - d0   
             
-        total_cost = current_bro.cost
+        total_cost = current_bro.cost * delta
         if total_cost != abs(total_cost):
             return dict(bro = False)
+        
         total_cost = str(total_cost)
         current_bro.stock = False #Change stock
         current_bro.booked_details = [Fname, Lname, str(curr_date.strftime("%B")) + " " + str(curr_date.day) + ", " + str(curr_date.year), date_, total_cost] #Store the booked details in the object
@@ -113,21 +115,24 @@ def return_product():
 @route('/return_success', method = "POST")
 @view('return_success')
 def return_success():
-    #Get form data entries
-    Fname = request.forms.get("first_name")
-    Lname = request.forms.get("last_name")
-    
-    #Find bro object by name
-    found_purchase = None
-    for bro in bros:
-        if bro.stock == False: 
-            if bro.booked_details[0] == Fname and bro.booked_details[1] == Lname:
-                found_purchase = bro
-                break
-    if found_purchase == None:  
-        return dict(bro = False)
-    found_purchase.stock = True #Change stock
-    return dict(bro = found_purchase) #Return found_bro to page
+    try:
+        #Get form data entries
+        Fname = request.forms.get("first_name")
+        Lname = request.forms.get("last_name")
+        
+        #Find bro object by name
+        found_purchase = None
+        for bro in bros:
+            if bro.stock == False: 
+                if bro.booked_details[0] == Fname and bro.booked_details[1] == Lname:
+                    found_purchase = bro
+                    break
+        if found_purchase == None:  
+            return dict(bro = False)
+        found_purchase.stock = True #Change stock
+        return dict(bro = found_purchase) #Return found_bro to page
+    except:
+        return dict(bro = False)    
 
 #Application page
 @route('/application.html')
